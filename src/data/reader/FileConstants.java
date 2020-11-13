6e375ffbd52626e959d00b99e26fb91cb89eea35
Marian Stochanskyi <stochan.andr@gmail.com>
2020-11-13 14:14:45 +0200

    Move text file to project folder

--- src/data/reader/FileConstants.java
+++ src/data/reader/FileConstants.java
@@ -1,5 +1,5 @@
 package data.reader;
 
 public class FileConstants {
-    public static final String filePath = "/Users/Marian/NULP/Course3/Term1/Cross-platform/Lab3/testText.txt";
+    public static final String filePath = "testText.txt";
 }
